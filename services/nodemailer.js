4a58eb3b8e9a8db50785b03c34608375c19601b0
Ismail Hakki Yildiz <hakki@MBPvonIsmail612.fritz.box>
2025-03-05 00:07:53 +0100

    Adding Handlebars 7.0.0 in the project

--- services/nodemailer.js
+++ services/nodemailer.js
@@ -1,53 +1,50 @@
-const nodeMailer = require ('nodemailer')
-const hbs = require ('nodemailer-express-handlebars')
+const nodeMailer = require('nodemailer');
 
-
+// Dynamischer Import von nodemailer-express-handlebars
+const hbs = async () => {
+    return (await import('nodemailer-express-handlebars')).default;
+};
 
 exports.nodeMailerSend = async (emailReceiver) => {
-
     const transporter = nodeMailer.createTransport({
         host: 'smtp.strato.de',
         port: 465,
         secure: true,
-        auth:{
-            user:'webmaster@pinobeep.de',
-            pass: 'EoN1Px7hqTqF431'
-        }
-    })
+        auth: {
+            user: 'webmaster@pinobeep.de',
+            pass: 'EoN1Px7hqTqF431',
+        },
+    });
 
-    const hbsOptions  = {
-        viewEngine:{
+    const Handlebars = await hbs(); // Warte auf den dynamischen Import
+
+    const hbsOptions = {
+        viewEngine: {
             defaultLayout: 'views/emails/masterLayout',
             layoutDir: 'views/emails',
-            partialsDir: ["views/emails", "views/emails/css"],
+            partialsDir: ['views/emails', 'views/emails/css'],
         },
         viewPath: 'views/emails',
         extName: '.hbs',
-    } 
+    };
 
-    transporter.use('compile', hbs(hbsOptions))
+    transporter.use('compile', Handlebars(hbsOptions));
 
-
-    const mail_config ={
-        from: "PinoBeep <master@pinoBeep.de>",
-        to:emailReceiver,
-        subject:" TEST Email ",
+    const mailConfig = {
+        from: 'PinoBeep <master@pinoBeep.de>',
+        to: emailReceiver,
+        subject: 'TEST Email',
         template: 'welcomeMsg',
         context: {
-            userName : "Hakki Yildiz",
-            age: "22"
+            userName: 'Hakki Yildiz',
+            age: '22',
+        },
+    };
+
+    transporter.sendMail(mailConfig, function (error, info) {
+        if (error) {
+            console.log(error);
         }
-    }
-
-    transporter.sendMail(mail_config, function (error, info){
-        if (error){
-            console.log(error)
-            //return reject({ message: `An error has accured` })
-        }
-        console.log('Message is sent ')
-        //return resolve ({ message: `Email send successfuly` })
-    })
-
-    //console.log(info)
-}
-
+        console.log('Message is sent');
+    });
+};
