663260dfd1e50ca197668c99f211534963575946
Ismail Hakki Yildiz <hakki@MBPvonImail1944.fritz.box>
2026-04-07 01:38:05 +0200

    Remove .DS_Store from tracking; Change price and link to ebay

--- config/products.js
+++ config/products.js
@@ -3,7 +3,7 @@ module.exports = {
     sku: "pinobeep_two_color_45v",
     name: "PinoBeep in zwei Farben mit 4,5V",
     description: "PinoBeep Original in zwei Farben, mit 4,5V Batterie-Versorgung",
-    price: "29.99",
+    price: "24.99",
     currency: "EUR",
     imageName: "pinoBeep_img_promotion_001.jpg",
   },
